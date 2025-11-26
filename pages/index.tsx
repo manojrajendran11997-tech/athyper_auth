669e6b91e1e5041b1ad681f6d877ec1530609bb4
Manoj Rajendran <manoj.rajendran@atlasdigitaltech.com>
2025-11-26 14:51:28 +0530

    Redirect root to auth/login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
-import React from "react";
-import Link from "next/link";
-import AuthForm from "../components/AuthForm";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div>
-        <AuthForm mode="login" />
-        <div className="text-center mt-4">
-          <Link href="/register" className="text-blue-600">
-            Create an account
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/auth/login");
+  }, []);
+
+  return null;
 }
